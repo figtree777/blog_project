44c4c2f38a2e40567e06488c8eb90d65a45c161c
figtree777 <maximusfigueras@gmail.com>
2024-07-04 02:13:52 -0400

    create and list posts

--- main/post/urls.py
+++ main/post/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.app_test),
+    path('create/', views.create, name='create'),
+    path('list/', views.list, name='list'),
 ]
--- main/post/views.py
+++ main/post/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from .forms import PostForm
+from .models import Post
+
+def list(request):
+    posts = Post.objects.all()
+    return render(request, 'list.html', {'posts': posts})
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')  # Redirect to a view displaying all posts
+    else:
+        form = PostForm()
+    return render(request, 'create.html', {'form': form})
+
 def app_test(request):
     return HttpResponse("Hello, World!")
