2ed304669901db467061378cc7b13d5bdb45223d
figtree777 <maximusfigueras@gmail.com>
2024-07-06 02:41:51 -0400

    login service init and html formatting

--- main/main/urls.py
+++ main/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('post/', include(('post.urls', 'post'), namespace='post')),
-    path('', include(('home.urls', 'home'), namespace='home')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
