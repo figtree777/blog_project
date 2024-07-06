60c5b4c6966bf1099cb8e256faeec625e070b1c4
figtree777 <maximusfigueras@gmail.com>
2024-07-06 02:45:51 -0400

    redundant

--- main/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def app_test(request):
-    return HttpResponse("Hello, World!")
-
-def home(request):
-    return render(request, 'home.html')
