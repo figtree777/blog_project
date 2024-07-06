5068e65efedf316e1e0cc57a9dc82d18aa21d3f0
figtree777 <maximusfigueras@gmail.com>
2024-07-06 07:29:28 -0400

    recent posts on homepage

--- main/main/urls.py
+++ main/main/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from post.models import Post
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('post/', include(('post.urls', 'post'), namespace='post')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html', extra_context={'posts': Post.objects.all()}), name='home'),
 ]
--- main/post/models.py
+++ main/post/models.py
@@ -4,4 +4,3 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
