4deaa0263917d6b05b88f06538d64d4c4ac69d90
figtree777 <maximusfigueras@gmail.com>
2024-07-08 14:24:52 -0400

    email required for signup

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -7,5 +9,19 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['email'] = forms.EmailField(required=True)
+        form.fields['username'] = forms.CharField(required=True)
+        form.fields['password1'].label = 'Password'
+        form.fields['password2'].label = 'Confirm Password'
+
+        return form
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
 
 
